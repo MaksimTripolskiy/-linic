5bd36fbccff431f297589c8e76dac4b0af54b9be
user <tripolskiy.02@mail.ru>
2026-03-26 21:09:48 +0300

    first commit

--- src/main/java/com/geoclinic/model/Clinic.java
+++ src/main/java/com/geoclinic/model/Clinic.java
@@ -1,12 +1,11 @@
 package com.geoclinic.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
-import java.util.Objects;
 
+
+@Data
 @Entity
 @Table(name="clinics")
 public class Clinic {
@@ -15,48 +14,10 @@ public class Clinic {
     private Long id;
     @Column(name="name")
     private String name;
-    @Column(name="address")
-    private String address;
     @Column(name="type")
     private String type;
+    @OneToOne
+    private Coordinates coordinates;
 
     public Clinic() {}
-
-    @Override
-    public String toString() {
-        return "Clinic{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", type='" + type + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Clinic clinic = (Clinic) o;
-        return Objects.equals(id, clinic.id) && Objects.equals(name, clinic.name) && Objects.equals(address, clinic.address) && Objects.equals(type, clinic.type);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, address, type);
-    }
 }
--- src/main/java/com/geoclinic/model/Coordinates.java
+++ src/main/java/com/geoclinic/model/Coordinates.java
@@ -1,13 +1,25 @@
 package com.geoclinic.model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data           // todo toString() & equals() needed?
+@Entity
+@Table(name="coordinates")
 public class Coordinates {
 
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "address")
     private String address;
+    @Column(name = "x")
     private double x;
+    @Column(name = "y")
     private double y;
 
 }
