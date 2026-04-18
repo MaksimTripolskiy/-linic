7e8c53930ddd827e83d533fcc20dcb4e763a87af
user <tripolskiy.02@mail.ru>
2026-04-18 18:09:29 +0300

    Continue creat comment

--- src/main/java/com/geoclinic/controller/CommentController.java
+++ src/main/java/com/geoclinic/controller/CommentController.java
@@ -1,33 +1,33 @@
-//package com.geoclinic.controller;
-//
-//import com.geoclinic.model.Comment;
-//import com.geoclinic.model.CommentStatus;
-//import com.geoclinic.service.CommentService;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//@RestController
-//public class CommentController {
-//
-//    private CommentService commentService;
-//
-//    public CommentController(CommentService commentService) {
-//        this.commentService = commentService;
-//    }
-//
-//    @PostMapping("/createPendingComment")
-//    public String createPendingComment(@ModelAttribute Comment comment, Model model) {
-//        try {
-//            // Сохранить клинику
-//            comment.setStatus(CommentStatus.PENDING);
-//            commentService.addComment(comment);
-//            model.addAttribute("message", "Comment created successfully!");
-//        } catch (Exception e) {
-//            model.addAttribute("error", "Error: " + e.getMessage());
-//        }
-//        return "clinic-page"; // вернуть ту же страницу с сообщением
-//    }
-//}
+package com.geoclinic.controller;
+
+import com.geoclinic.model.Comment;
+import com.geoclinic.model.CommentStatus;
+import com.geoclinic.service.CommentService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/leaveComment")
+    public String createPendingComment(@ModelAttribute Comment comment, Model model) {
+        try {
+            // Сохранить клинику
+            comment.setStatus(CommentStatus.PENDING);
+            commentService.addComment(comment);
+            model.addAttribute("message", "Comment created successfully!");
+        } catch (Exception e) {
+            model.addAttribute("error", "Error: " + e.getMessage());
+        }
+        return "clinic-page"; // вернуть ту же страницу с сообщением
+    }
+}
--- src/main/java/com/geoclinic/model/Comment.java
+++ src/main/java/com/geoclinic/model/Comment.java
@@ -1,11 +1,16 @@
 package com.geoclinic.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Table(name="comments")
+@Entity
 public class Comment {
 
     private String user;
@@ -13,5 +18,8 @@ public class Comment {
     private LocalDateTime dateTime;
     private int clinicId;
     private CommentStatus status;
+    private int rating;
+
+    public Comment() {}
 
 }
